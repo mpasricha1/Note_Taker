1279c8a11bd0d0d96c6f33c95ebe92a8fb9623a7
Mark Pasricha <markp862@gmail.com>
2021-02-22 21:43:17 -0500

    Cleaning up server file

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const http = require("http");  
 const express = require("express"); 
 
-const db = require('./db/db.json');
-
 const app = express();
 const PORT = 5000; 
 
@@ -13,29 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./routes/routes")(app);
-
-// app.get('/', (req, res) =>{
-// 	res.sendFile(path.join(__dirname, "public/index.html")); 
-// }); 
-
-// app.get('/notes', (req, res) =>{
-// 	res.sendFile(path.join(__dirname, "public/notes.html"));
-// 	console.log(req.body)
-// });
-
-// app.get('/api/notes', (req, res) =>{
-// 	return res.json(db);
-// });
-
-// app.post('/api/notes', (req, res) => {
-// 	let newNote = req.body; 
-
-// 	console.log(newNote); 
-// });
-
-// app.delete('/api/notes/:id'), (req, res) => {
-// 	res.send("Delete request")
-// };
 
 app.listen(PORT, () =>{
 	console.log(`Listening on Port: ${PORT}`); 
