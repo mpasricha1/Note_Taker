c5463f63e516260b74942911a9820d7d4d470fe2
Mark Pasricha <markp862@gmail.com>
2021-02-22 20:31:05 -0500

    Fixed issue with css and js files

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const http = require("http");
 const fs = require("fs"); 
 const express = require("express"); 
 const path = require("path");
-const db = require('../../../db/db.json');
+const db = require('./db/db.json');
 
 const app = express();
-const PORT = 8080; 
+const PORT = 5000; 
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('./'));
 
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 
 app.get('/', (req, res) =>{
-	res.sendFile(path.join(__dirname, "../../index.html")); 
+	res.sendFile(path.join(__dirname, "public/index.html")); 
 }); 
 
 // app.use('*', (req, res) =>{
@@ -19,16 +22,17 @@ app.get('/', (req, res) =>{
 // });
 
 app.get('/notes', (req, res) =>{
-	res.sendFile(path.join(__dirname, "../../notes.html"));
+	res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.get('/api/notes', (req, res) =>{
-	res.header("Content-Type", 'application/json'); 
-	res.send(JSON.stringify(db));
+	return res.json(db);
 });
 
 app.post('/api/notes', (req, res) => {
-	res.send("POST Request")
+	let newNote = req.body; 
+
+	console.log(newNote); 
 });
 
 app.delete('/api/notes/:id'), (req, res) => {
